7052297751cd22d686171b038f5376871f54378b
Rodrigo Pimentel satyro <xitarps@gmail.com>
2020-04-24 02:25:01 -0300

    add create and add messages to form if errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,4 +10,16 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.new(article_params)
+    return redirect_to @article if @article.save
+    render :new
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
