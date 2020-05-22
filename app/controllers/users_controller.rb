ec89930b4409c4808ce7bcff930cfbdd496a8f00
Rodrigo Pimentel satyro <xitarps@gmail.com>
2020-05-22 03:52:12 -0300

    Add user signup and edit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  def new
+    @user =User.new()
+  end
+
+  def create
+    @user = User.new(user_params)
+    msg = "You have successfully signed up #{@user.username}".to_sym
+    return redirect_to @user, notice: msg  if @user.save
+
+    render :new
+  end
+
+  def edit
+    @user =User.find(params[:id])
+  end
+
+  def update
+    @user =User.find(params[:id])
+    msg = "You have successfully updated your data".to_sym
+    return redirect_to @user, notice: msg if @user.update(user_params)
+
+    render :edit
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
