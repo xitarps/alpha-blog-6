915701a652f8b082c2c0b76fb6a5b58d9a6afb1d
Rodrigo Pimentel satyro <xitarps@gmail.com>
2020-05-23 11:23:02 -0300

    many to many association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
+  has_many :article_categories
+  has_many :articles, through: :article_categories
   validates :name, presence: true, uniqueness: true, length: {minimum: 5, maximum: 20}
 end
