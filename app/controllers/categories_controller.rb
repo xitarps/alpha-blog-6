a4deb9a6a1d9f28210a2ba8353c6d5c5010aeddd
Rodrigo Pimentel satyro <xitarps@gmail.com>
2020-05-24 04:44:30 -0300

    Add edit category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -20,6 +20,21 @@ class CategoriesController < ApplicationController
     @articles = @category.articles.paginate(page: params[:page], per_page: 5).order('created_at DESC')
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = 'Category successfully updated'
+      redirect_to @category
+    else
+      render :edit
+    end
+    
+  end
+
   private
 
   def category_params
